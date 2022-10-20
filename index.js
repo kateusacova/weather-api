e05e5a1b715d3b1d6a7f8b8fe32b230aea732ae6
kateusacova <kateusacova@gmail.com>
2022-10-20 13:48:41 +0100

    New class with displayWeather method

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
 class WeatherApi {
   constructor() {
     this.got = require('got');
     this.apiKey = require("./apiKey");
+    this.weatherData;
   }
 
   fetchWeatherData(city, callback) {
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${this.apiKey}`;
+
+    this.got(apiUrl).then((response) => {
+      this.weatherData = JSON.parse(response.body);
+      callback(this.weatherData);
+    });
+  }
+}
+
+class WeatherView {
+  
+  constructor() {
+    this.got = require('got');
+    this.apiKey = require("./apiKey");
+    this.rl = readline.createInterface({ input, output });
+  } 
+
+  getCity = () => {
+    this.rl.question('Please select city\n', (answer) => {
+      this.displayWeather(answer);
+      this.rl.close();
+    });
+  }
+
+  displayWeather(city) {
     let apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${this.apiKey}`;
     let weatherData = null;
 
     this.got(apiUrl).then((response) => {
       weatherData = JSON.parse(response.body);
-      callback(weatherData);
+      console.log(`City: ${weatherData.name}\n`,
+        `Weather: ${weatherData.weather[0].main}\n`,
+        `Temperature: ${weatherData.main.temp}\n`,
+        `Feels like: ${weatherData.main.feels_like}\n`,
+        `Humidity: ${weatherData.main.humidity}`);
     });
   }
 }
@@ -33,3 +66,16 @@ class Weather {
 }
 
 module.exports = { WeatherApi, Weather }
+
+// const view = new WeatherView;
+// view.getCity();
+
+
+
+
+
+
+
+
+
+
